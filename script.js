75f3ccb27fdce807da0a163aaddee340d67d0d2b
Yuliya Tsikhanovich <ladi.89@mail.ru>
2024-03-19 22:51:44 +0300

    work fix

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-//  const {body} = document
-//   const swipers = document.querySelectorAll('.swiper')
-//   body.onresize = () => {
-//     console.log(body.offsetWidth)
-//     swipers.forEach(it => {
-//       if(body.offsetWidth > 768) {
-//         //it.classList.remove('d-none')
-//         it.style = ""
-//       } else {
-//         //it.classList.add('d-none')
-//         it.style = "display: none"
-//       }
-//     })
-//   }
-
+//слайдер свайп
 
 function initSlider() {
     new Swiper(".mySwiper", {
@@ -28,51 +14,28 @@ initSlider();
 
 
 
-// document.getElementById("read__more").addEventListener("click", function() {
-//   document.getElementById("services-hidden-third").style.display = "block";
-// });
 
+const showMoreButton = document.getElementById('btn1');
+const hideButton = document.getElementById('btn2');
 
-// const toggleButton = document.getElementById('read__more');
-// const block = document.getElementById('services-hidden-third');
-
-// toggleButton.addEventListener('click', function() {
-//   if (block.style.display === 'none') {
-//     block.style.display = 'flex';
-//   } else {
-//     block.style.display = 'none';
-//   }
-// });
-
-
-
-document.getElementById("btn1").addEventListener("click", function(){
-  document.getElementById("services-hidden-third").style.display = "flex";
+showMoreButton.onclick = () => {
+  const elements = document.querySelectorAll('.services-slide');
+  elements.forEach((element, index) => {
+    if (index >= 8) {
+      element.style.display = 'block';
+    }
+  });
   document.getElementById("btn1").style.display = "none";
   document.getElementById("btn2").style.display = "block";
-});
+};
 
-document.getElementById("btn2").addEventListener("click", function(){
-  document.getElementById("services-hidden-third").style.display = "none";
+hideButton.onclick = () => {
+  const elements = document.querySelectorAll('.services-slide');
+  elements.forEach((element, index) => {
+    if (index >= 8) {
+      element.style.display = 'none';
+    }
+  });
   document.getElementById("btn1").style.display = "block";
   document.getElementById("btn2").style.display = "none";
-});
-
-const elements = document.querySelectorAll('.services-hidden');
-
-document.getElementById("btn1").addEventListener("click", function(){
-  elements.forEach(element => {
-   element.style.display = 'block';
-});
-  document.getElementById("btn1").style.display = "none";
-  document.getElementById("btn2").style.display = "block";
-});
-
-document.getElementById("btn2").addEventListener("click", function(){
-  document.querySelectorAll('.services-hidden').forEach(element => {
-  element.style.display = 'none';
-});
-  document.getElementById("btn1").style.display = "block";
-  document.getElementById("btn2").style.display = "none";
-});
-
+};
