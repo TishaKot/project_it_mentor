e5543c3e9b240ba17225f3d96b7ea64327b12a91
Yuliya Tsikhanovich <ladi.89@mail.ru>
2024-05-04 01:20:41 +0300

    final work

--- script.js
+++ script.js
@@ -1,77 +1,18 @@
-//слайдер свайп
 
-function initSlider() {
-    new Swiper("#swiper1", {
-      slidesPerView: "auto",
-        spaceBetween: 15,
-        loop: false,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-          dynamicBullets: true,
-          spaceBetween: 40,
-        },
-        mousewheel: true,
-        keyboard: true,
-      });
-}
-initSlider();
-
-
- function initSlider2() {
-    new Swiper("#swiper2", {
-     slidesPerView: "auto",
-        spaceBetween: 10,
-        loop: false,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-          dynamicBullets: true,
-          type: "bullets",
-        },
-        mousewheel: true,
-        keyboard: true,
-      });
-}
-initSlider2();
-
-function initSlider3() {
-    new Swiper("#swiper3", {
-      slidesPerView: 1,
-        spaceBetween: 10,
-        loop: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-          dynamicBullets: true,
-          spaceBetween: 20,
-        },
-        
-        mousewheel: true,
-        keyboard: true,
-        
-      });
-}
-initSlider3();
-
-
-
-//Функция показа блока РЕМОНТ БРЕНДОВ
+//Блок РЕМОНТ БРЕНДОВ
 
 const container = document.getElementById('container');
-const elements = container.querySelectorAll('.services-slide');
+const elements = document.querySelectorAll('.services__brands__slide');
 const btn = document.getElementById('btn');
-const btnImg = document.querySelector('.btn__img');
-const text = document.querySelector('.text');
+const btnImg = document.querySelector('.services__btn__img');
+const text = document.querySelector('.services__btn__text');
 
 let showAll = false;
 
 function toggleElements() {
-  // Задаем количество элементов, которые нужно показывать в зависимости от разрешения экрана
   const screenWidth = window.innerWidth;
   let numToShow = screenWidth >= 1120 ? 8 : screenWidth >= 768 ? 6 : 0;
 
-  // Показываем или скрываем элементы в зависимости от значения numToShow
   elements.forEach((element, index) => {
     if (numToShow === 0 || index < numToShow || showAll) {
       element.style.display = 'block';
@@ -80,43 +21,31 @@ function toggleElements() {
     }
   });
 
-
-  // Меняем текст на кнопке в зависимости от showAll
   if (showAll) {
     text.textContent = 'Скрыть';
     btnImg.style.transform = 'rotate(180deg)';
-    
   } else {
     text.textContent = 'Показать все';
     btnImg.style.transform = 'rotate(0deg)';
-
   }
 }
-
-
 
 btn.addEventListener('click', () => {
   showAll = !showAll;
   toggleElements();
 });
 
-
-// Вызываем функцию toggleElements для первоначального отображения элементов
 toggleElements();
 
-
-
-// При изменении размера окна браузера также пересчитываем отображаемые элементы
 window.addEventListener('resize', toggleElements);
 
 
 function hideButtonOnMobile() {
-    if (window.innerWidth < 768) {
-        btn.style.display = "none";
-    } else {
-       btn.style.display = "block";
-    }
-    
+  if (window.innerWidth < 768) {
+    btn.style.display = "none";
+  } else {
+    btn.style.display = "block";
+  }
 }
 
 hideButtonOnMobile();
@@ -124,32 +53,21 @@ window.addEventListener('resize', hideButtonOnMobile);
 
 
 
-// Функция показа блока РЕМОНТ ТЕХНИКИ
+// Блок РЕМОНТ ТЕХНИКИ
 
-const blocks = container.querySelectorAll('.technique__block');
-const btnT = document.querySelector('.technique__btn');
-const btnImges = document.querySelector('.technique__btn__img');
-const textBtn = document.querySelector('.technique__btn__text');
+const blocks = document.querySelector('.services__technique__blocks');
+const elem = document.querySelectorAll('.technique__block');
+const btnServices = document.getElementById('btn_services');
+const btnImges = document.querySelector('.btn__img');
+const textBtn = document.querySelector('.btn_services__text');
 
 let show = false;
 
+function toggleElem() {
+  const screenWid = window.innerWidth;
+  let numTo = screenWid >= 1120 ? 4 : screenWid >= 768 ? 3 : 0;
 
- btnT.addEventListener("click", function() {
-            let hiddenElements = document.querySelectorAll(".technique__block_hidden");
-            hiddenElements.forEach(function(element) {
-                element.style.display = "block";
-            });
-        });
-
-        function toggleButton() {
-
-
-// Задаем количество элементов, которые нужно показывать в зависимости от разрешения экрана
-  const screenW = window.innerWidth;
-  let numTo = screenW >= 1120 ? 4 : screenW >= 768 ? 3 : 0;
-
-  // Показываем или скрываем элементы в зависимости от значения numToShow
-  blocks.forEach((element, index) => {
+  elem.forEach((element, index) => {
     if (numTo === 0 || index < numTo || show) {
       element.style.display = 'block';
     } else {
@@ -158,67 +76,79 @@ let show = false;
   });
 
 
-    // Меняем текст на кнопке в зависимости от showAll      
- if (show) {
+  if (show) {
     textBtn.textContent = 'Скрыть';
     btnImges.style.transform = 'rotate(180deg)';
-    
+
   } else {
     textBtn.textContent = 'Показать все';
-   btnImges.style.transform = 'rotate(0deg)';
-
+    btnImges.style.transform = 'rotate(0deg)';
   }
-    }
+}
 
-
-btnT.addEventListener('click', () => {
+btnServices.addEventListener('click', () => {
   show = !show;
-  toggleButton();
+  toggleElem();
 });
 
+toggleElem();
 
-// Вызываем функцию toggleElements для первоначального отображения элементов
-toggleButton();
+window.addEventListener('resize', toggleElem);
 
 
-// Открытие модального окна ОБРАТНАЯ СВЯЗЬ
+function hideButtonOnM() {
+  if (window.innerWidth < 768) {
+    btnServices.style.display = "none";
+  } else {
+    btnServices.style.display = "block";
+  }
+}
+
+hideButtonOnM();
+window.addEventListener('resize', hideButtonOnM);
+
+
+
+// модальное окно ОБРАТНАЯ СВЯЗЬ
 
 const modal = document.querySelector('.modal');
 const openModalButton = document.querySelector('.icon__chat');
 const closeModalButton = document.getElementById('modal__window_call_btn_close');
 const modalContent = document.querySelector('.modal__window__feedback');
-const headerPhone = document.querySelector('.phone');
 
-openModalButton.addEventListener("click", function() {
-    modal.style.display = "block";
+const headerChat = document.querySelector('.chat');
+
+
+openModalButton.addEventListener("click", function () {
+  modal.style.display = "block";
 });
 
-closeModalButton.addEventListener("click", function() {
+closeModalButton.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
     modal.style.display = "none";
+  }
 });
 
-modal.addEventListener("click", function(e) {
-    if (e.target === modal) {
-        modal.style.display = "none";
-    }
-});
+// на <= 768  Открытие модального окна ОБРАТНАЯ СВЯЗЬ
 
-// на 768 Открытие модального окна ОБРАТНАЯ СВЯЗЬ
-
-headerPhone.addEventListener("click", function() {
-    modal.style.display = "flex";
-   modalContent.style.display = "flex";
+headerChat.addEventListener("click", function () {
+  modal.style.display = "flex";
+  modalContent.style.display = "flex";
 });
 
 
-closeModalButton.addEventListener("click", function() {
+closeModalButton.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
     modal.style.display = "none";
-});
-
-modal.addEventListener("click", function(e) {
-    if (e.target === modal) {
-        modal.style.display = "none";
-    }
+  }
 });
 
 
@@ -226,16 +156,16 @@ modal.addEventListener("click", function(e) {
 
 const modalD = document.querySelector('.modal__wrap__call');
 const phone = document.querySelector('.icon__call');
-const headerChat = document.querySelector('.chat');
+const headerPhone = document.querySelector('.phone');
 const closeButton = document.getElementById('modal__window_btn_close');
 
 
-phone.addEventListener("click", function() {
-    modalD.style.display = "flex";
-   
+phone.addEventListener("click", function () {
+  modalD.style.display = "flex";
+
 });
 
-closeButton.addEventListener('click', function() {
+closeButton.addEventListener('click', function () {
   modalD.style.display = 'none';
 });
 
@@ -246,24 +176,22 @@ modalD.addEventListener('click', (e) => {
 });
 
 
+// на <= 768 Открытие модального окна ЗАКАЗАТЬ ЗВОНОК
 
-// на 768 Открытие модального окна ЗАКАЗАТЬ ЗВОНОК
+headerPhone.addEventListener("click", function () {
+  modalD.style.display = "flex";
 
-headerChat.addEventListener("click", function() {
-    modalD.style.display = "flex";
-   
 });
 
-closeButton.addEventListener('click', function() {
+closeButton.addEventListener('click', function () {
   modalD.style.display = 'none';
 });
 
 modalD.addEventListener('click', (e) => {
   if (e.target === modalD) {
-   modalD.style.display = 'none';
+    modalD.style.display = 'none';
   }
 });
-
 
 
 // функция открытия текста
@@ -272,64 +200,54 @@ const readButton = document.querySelector('.services__button');
 const readText = document.querySelector('.text__screen_768__end');
 const imgArrow = document.querySelector('.img__arrow');
 const buttonText = document.querySelector('.services__button__text');
+const text768 = document.querySelector('.text__screen_768');
+const screenWidth = window.innerWidth;
 
-
-readButton.addEventListener('click', function() {
+readButton.addEventListener('click', function () {
   if (readText.classList.contains('hidden')) {
-     readText.classList.remove('hidden');
-    buttonText.textContent  = 'Скрыть';
+    readText.classList.remove('hidden');
+    buttonText.textContent = 'Скрыть';
     imgArrow.style.transform = 'rotate(180deg)';
   } else {
-     readText.classList.add('hidden');
-      buttonText.textContent  = 'Читать далее';
-   imgArrow.style.transform = 'rotate(0deg)';
+    readText.classList.add('hidden');
+    buttonText.textContent = 'Читать далее';
+    imgArrow.style.transform = 'rotate(0deg)';
+  }
+});
+
+readButton.addEventListener('click', function (event) {
+  if (text768.style.display === "block") {
+    text768.style.display = "none";
+  } else {
+    event.preventDefault();
+    text768.style.display = "block";
+    readText.style.display = "block";
   }
 });
 
 
-// бургер меню
+
+// BURGER MENU
 
 
-
-////////////////////////////////////////////
-// document.addEventListener('DOMContentLoaded', function () {
-//     const burgerBtn = document.getElementById('burger__btn');
-//     const burgerMenu = document.querySelector('.burger__menu');
-    
-//     burgerBtn.addEventListener('click', function() {
-//         burgerMenu.style.left = '0';
-//     });
-
-//     burgerMenu.addEventListener('click', function() {
-//         burgerMenu.style.left = '-320px';
-//     });
-
-//     burgerMenu.addEventListener('mouseleave', function() {
-//         burgerMenu.style.left = '-320px';
-//     });
-// });
-
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const burgerBtn = document.getElementById('burger__btn');
   const closeBtn = document.querySelector('.burger__close-btn');
   const burgerMenu = document.querySelector('.burger__menu');
   const overlay = document.querySelector('.burger__overlay');
 
-  burgerBtn.addEventListener('click', function() {
+  burgerBtn.addEventListener('click', function () {
     burgerMenu.classList.add('open');
     overlay.style.display = 'block';
   });
 
-  closeBtn.addEventListener('click', function() {
+  closeBtn.addEventListener('click', function () {
     burgerMenu.classList.remove('open');
     overlay.style.display = 'none';
   });
 
-  overlay.addEventListener('click', function() {
+  overlay.addEventListener('click', function () {
     burgerMenu.classList.remove('open');
     overlay.style.display = 'none';
   });
-
-  
 });
